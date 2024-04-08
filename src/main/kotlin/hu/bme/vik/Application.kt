d0933bae70e410776b638249a66abc298d9ed765
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2024-04-08 15:49:50 +0200

    health check fix

--- src/main/kotlin/hu/bme/vik/Application.kt
+++ src/main/kotlin/hu/bme/vik/Application.kt
@@ -2,6 +2,8 @@ package hu.bme.vik
 
 import hu.bme.vik.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -10,4 +12,10 @@ fun main(args: Array<String>) {
 fun Application.module() {
     configureSerialization()
     configureRouting()
+
+    routing {
+        get("/health") {
+            call.respond("Up and running.")
+        }
+    }
 }
